a2ee3b469a96538b24dddeb2a47580d16c325edb
LeyzerGurewitz <leyzer.3770@gmail.com>
2024-09-01 11:26:54 +0300

    create selectionSort

--- looked for/Program.cs
+++ looked for/Program.cs
@@ -1,7 +1,28 @@
-﻿namespace looked_for
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace looked_for
 {
     internal class Program
     {
+        public static int[] selectionSort(int[] selection)
+        {
+            int variable = 0;
+            for (int i = 0; i < selection.Length; i++)
+            {
+
+                for (int j = i; j < selection.Length; j++)
+                {
+
+                    if (selection[j] < selection[i])
+                    {
+                        variable = selection[j];
+                        selection[j] = selection[i];
+                        selection[i] = variable;
+                    }
+                }
+            }
+            return selection;
+        }
         public static int[] LookedFor(int[] arr)
         {
             int x = 0;
@@ -26,6 +47,9 @@
             Console.WriteLine("Hello, World!");
 
             int[] sortedArray = LookedFor(new int[] { 5, 3, 8, 4, 1 });
+            Console.WriteLine(string.Join(", ", sortedArray));
+
+            int[] sortedArray1 = selectionSort(new int[] { 5, 3, 8, 4, 1 });
             Console.WriteLine(string.Join(", ", sortedArray));
         }
     }
--- looked for/obj/Debug/net8.0/looked for.AssemblyInfo.cs
+++ looked for/obj/Debug/net8.0/looked for.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("looked for")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3439dc8e27abaef796bf326520f3bc2d486ce97a")]
 [assembly: System.Reflection.AssemblyProductAttribute("looked for")]
 [assembly: System.Reflection.AssemblyTitleAttribute("looked for")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
